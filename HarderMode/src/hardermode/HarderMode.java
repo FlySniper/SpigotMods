efe8e6b045e6aff91180299f976f78b50781904f
BenRajcan <brajcan@hotmail.com>
2015-08-17 15:18:14 -0400

    Zombies need a slight tweak, make them weakness instead of resistance.
    Skeletons cannot inflict status effects with their arrows.
    
    When the server restarts, torches may remain alive forever.

--- HarderMode/src/hardermode/HarderMode.java
+++ HarderMode/src/hardermode/HarderMode.java
@@ -1,10 +1,17 @@
 package hardermode;
 
+import java.util.Iterator;
+import java.util.List;
+
 import hardermode.entity.*;
 import hardermode.block.*;
 
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,6 +20,25 @@ public class HarderMode extends JavaPlugin implements Listener {
 	public void onEnable()
 	{
 		Server server = this.getServer();
+		List<Recipe> recipeIterator = server.getRecipesFor(new ItemStack(Material.TORCH));
+		
+		server.clearRecipes();
+		for(Recipe r : recipeIterator)
+		{
+			ItemStack res = r.getResult();
+			if(res.getType() == Material.TORCH && res.getAmount() == 4)
+			{
+				ShapedRecipe torchrecipe = new ShapedRecipe(new ItemStack(Material.TORCH,16));
+				if(r instanceof ShapedRecipe)
+				{
+					String [] shape = ((ShapedRecipe)r).getShape();
+					torchrecipe.shape(shape);
+				}
+				server.addRecipe(torchrecipe);
+				continue;
+			}
+			server.addRecipe(r);
+		}
 		PluginManager pluginmanager = server.getPluginManager();
 		pluginmanager.registerEvents(this, this);
 		pluginmanager.registerEvents(new ZombieEvents(),this);
